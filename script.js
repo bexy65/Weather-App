1c6ff6319553db756ab7b9475dfcff44fa3f87be
bexy65 <naimertan65@gmail.com>
2023-11-21 11:26:45 +0200

    refactor

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
-const api = 'edcc4d09c628262de0a35e3b8054e26f';
-const lat = '42.697708';
-const lon = '23.321867';
-const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lon}&appid=${api}`;
+const apiKey = 'edcc4d09c628262de0a35e3b8054e26f';
 
-//Selecting HTML elements
-const cityName = document.getElementById('cityName');
-const degrees = document.getElementById('degrees');
-const feelsLike = document.getElementById('feelsLike');
-const clouds = document.getElementById('clouds');
-const cloudIcon = document.getElementById('cloudIcon');
-const wind = document.getElementById('wind');
+const cityNameEl = document.getElementById('city');
+const tempEl = document.getElementById('temp');
+const weatherInfoEl = document.getElementById('weatherInfo');
+const weatherDescEl = document.getElementById('weatherDescription');
+const weatherIconEl = document.getElementById('weatherIcon');
 
-const assets = {
-  cloudy: 'Cloudy',
-  clear: 'Clear',
+const weatherIcon = function weatherIcon(iconCode) {
+  return `http://openweathermap.org/img/w/${iconCode}.png`;
 };
 
+//get location
+async function getLocation() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lng = position.coords.longitude;
+        const lat = position.coords.latitude;
+        const url = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${lat}&lon=${lng}&appid=${apiKey}`;
+
+        fetchAndRender(url);
+      },
+      (error) => {
+        console.error('Error getting user location:', error);
+      }
+    );
+  } else {
+    console.error('Geolocation is not supported by this browser.');
+  }
+}
+
+getLocation();
+
 //fetch from api
-const message = document.getElementById('message');
-async function checkWeather() {
+async function fetchAndRender(url) {
   const res = await fetch(url);
   const data = await res.json();
-
-  cityName.textContent = data.name;
-  degrees.textContent = `${data.main.temp} C°`;
-  clouds.textContent = data.clouds.all > 50 ? assets.cloudy : assets.clear;
-  wind.textContent = data.wind.speed;
-
+  renderData(data);
   console.log(data);
 }
 
-checkWeather();
+function renderData(resData) {
+  cityNameEl.textContent = resData.name;
+  tempEl.textContent = resData.main.temp;
+  weatherInfoEl.textContent = resData.weather[0].main;
+  weatherDescEl.textContent = resData.weather[0].description;
+}
 
+//weather: [ { id: 800, main: 'Clear', description: 'clear sky', icon: '01n' } ],
+//weather[0].icon,
+//weather[0].main,
+//weather[0].description,
+
+//base: 'stations',
 //display these features in UI
 //data.main.temp - temp_max/temp_min
 //data.main.feels_like
 //data.name
-//data.weather[]
 //data.weather[0].description
 //data.weather[0].icon
 //data.wind
